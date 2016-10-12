2d3dbf25c4f0d377554acb7a28fd468396c4a1dd
jqj8595 <jqj8595@autuni.ac.nz>
2016-10-12 16:09:00 +1300

    lets do this

---
+++ app/src/main/java/com/umenu/umenu/Testinggithub.java
@@ -0,0 +1,17 @@
+package com.umenu.umenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Testinggithub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testinggithub);
+
+        
+
+
+    }
+}
