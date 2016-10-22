720440340027f61dff77ee0606029bbd0a24d2ab
jqj8595 <jqj8595@autuni.ac.nz>
2016-10-23 10:18:10 +1300

    Can now log in through facebook

--- app/src/main/java/com/umenu/umenu/MainActivity.java
+++ app/src/main/java/com/umenu/umenu/MainActivity.java
@@ -1,10 +1,8 @@
 package com.umenu.umenu;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.facebook.CallbackManager;
@@ -18,7 +16,7 @@ import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONObject;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity{
     CallbackManager callbackManager;
 
     @Override
@@ -26,79 +24,72 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         facebookSDKInitialize();
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email,publish_actions");
-        getLoginDetails(loginButton);
-
-
-    }
-
-
-
-
-    /*
-    Initialize the facebook sdk and then callback manager will handle the login responses.
- */
-    protected void facebookSDKInitialize() {
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-
         callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email,publish_actions");//grabs facebook profile permission
+        getFacebook_LoginDetails(loginButton); //calls login details
     }
 
 
-        /*
-    Register a callback function with LoginButton to respond to the login result.
-    On successful login,login result has new access token and  recently granted permissions.
-    */
+    /**
+     * initializes facebook sdk and gets the application context for the login feature
+     */
+    protected void facebookSDKInitialize() {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
 
-    protected void getLoginDetails(LoginButton login_button){
 
+    /**
+     * log in button is used for access through the app. if user is identified calls on
+     * getUserInfo to retrieve users profile information for the purpose of the application.
+     * @param login_button
+     */
+
+    protected void getFacebook_LoginDetails(LoginButton login_button){
         // Callback registration
         login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onSuccess(LoginResult login_result) {
-
-
-                getUserInfo(login_result);
-
-
+            public void onSuccess(LoginResult login_result) { //if users login is successful or not
+                getFacebook_UserInfo(login_result);
             }
-
             @Override
-            public void onCancel() {
-                // code for cancellation
+            public void onCancel() { //cancelling process automatically takes you back
             }
-
             @Override
-            public void onError(FacebookException exception) {
-                //  code to handle error
+            public void onError(FacebookException exception) { //calls on facebook exception.
+
             }
         });
     }
 
 
-
+    /**
+     * This log in and sign up buttons are for the restaurants databse, for people not logging
+     * in using facebook or other social media, and want to create their own user profile using
+     * our databse.
+     * @param v
+     */
     public void onButtonClick(View v)
     {
         if(v.getId() == R.id.btnLogin)
         {
-            Intent intentLogin = new Intent(MainActivity.this,Homepage.class);
+            Intent intentLogin = new Intent(MainActivity.this,Homepage.class); //calls Homepage
             startActivity(intentLogin);
         }
         else if(v.getId() == R.id.btnSignup)
         {
-            Intent iSignup = new Intent(MainActivity.this,Signup.class);
+            Intent iSignup = new Intent(MainActivity.this,Signup.class); // calls sign up page
             startActivity(iSignup);
         }
-
-
     }
 
 
-    protected void getUserInfo(LoginResult login_result){
-
+    /**
+     * This method retrieves the login user information from facebook and calls uppon the Homepage
+     * activity once it has been successful
+     * @param login_result
+     */
+    protected void getFacebook_UserInfo(LoginResult login_result){
         GraphRequest data_request = GraphRequest.newMeRequest(
                 login_result.getAccessToken(),
                 new GraphRequest.GraphJSONObjectCallback() {
@@ -118,49 +109,21 @@ public class MainActivity extends Activity{
 
     }
 
-
-
+    /**
+     * On activity result used for call back manager and to use sdk as a superclass for current
+     * activity.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        // getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-        @Override
-        protected void onResume(){
-            super.onResume();
-
-            // Logs 'install' and 'app activate' App Events.
-
-        }
-
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        // Logs 'app deactivate' App Event.
-
-    }
 
 
 
