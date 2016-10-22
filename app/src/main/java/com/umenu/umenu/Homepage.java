8d9f537214e2cf96d408a5e0a686b2d2c8dd00a3
jqj8595 <jqj8595@autuni.ac.nz>
2016-10-23 01:22:41 +1300

    Facebook Login function for sharing..

--- app/src/main/java/com/umenu/umenu/Homepage.java
+++ app/src/main/java/com/umenu/umenu/Homepage.java
@@ -4,31 +4,49 @@ package com.umenu.umenu;
  * Created by nishanjayetileke on 11/10/16.
  */
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.share.ShareApi;
 import com.facebook.share.model.SharePhoto;
 import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -46,15 +64,35 @@ public class Homepage extends AppCompatActivity implements PopupMenu.OnMenuItemC
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    JSONObject response, profile_pic_data, profile_pic_url;
+    TextView user_name, user_email;
+    ImageView user_picture;
+    NavigationView navigation_view;
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+    int REQUEST_CAMERA =0, SELECT_FILE =1;
+
+
 
 
     //main running method that is automatically calls during runtime of the application
     @Override
     synchronized protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        facebookSDKInitialize();
         setContentView(R.layout.activity_homepage);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+        Intent intent = getIntent();
+        String jsondata = intent.getStringExtra("jsondata");
+        shareDialog = new ShareDialog(this);
+
+
+
+
+
+
+
+
+
 
         //test_hashKey(); //made for unit testing function
 
@@ -118,6 +156,17 @@ public class Homepage extends AppCompatActivity implements PopupMenu.OnMenuItemC
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+
+    /**
+     * method initializes facebook sdk and creates object for callbackmanager.
+     */
+    private void facebookSDKInitialize() {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+    }
+
+
     //method call to act as a on click listener for popup menu
     @Override
     public boolean onMenuItemClick(MenuItem item) {
@@ -136,11 +185,124 @@ public class Homepage extends AppCompatActivity implements PopupMenu.OnMenuItemC
     }
 
 
-    private void sharePhotoToFacebook() {
-        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.social_facebook_con);
+    private void selectImage(){
+        final CharSequence[] items = {"Take Photo", "Choose from Library", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(Homepage.this);
+        builder.setTitle("Select Profile Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if(items[item].equals("Take Photo")){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+
+
+                }
+                else if (items[item].equals("Choose from Library")){
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"), SELECT_FILE);
+
+                }
+                else if(items[item].equals("Cancel")){
+                    dialog.dismiss();;
+                }
+
+            }
+        });
+        builder.show();
+    }
+
+
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA){
+                onCaptureImageResult(data);
+            }
+
+        }
+    }
+
+
+    /**
+     * select photo from gallery
+     * @param data
+     */
+    private void onSelectFromGalleryResult(Intent data){
+        Uri selectedImageUri = data.getData();
+        String[] projection = {MediaStore.MediaColumns.DATA};
+        Cursor cursor = managedQuery(selectedImageUri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+
+        String selectedImagePath = cursor.getString(column_index);
+        Bitmap thumbnail;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(selectedImagePath, options);
+        final int Required_Size = 200;
+        int scale =1;
+        while(options.outWidth/scale/2>=Required_Size &&
+                options.outHeight/scale/ 2>= Required_Size){
+            scale *=2;
+            options.inSampleSize = scale;
+            options.inJustDecodeBounds = false;
+            thumbnail = BitmapFactory.decodeFile(selectedImagePath, options);
+
+            sharePhotoToFacebook(thumbnail);
+        }
+
+
+    }
+
+
+    /**
+     * take a photo and activate the camera on the device to upload photo
+     * to facebook profile.
+     * @param data
+     */
+
+    private void onCaptureImageResult(Intent data){
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try{
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sharePhotoToFacebook(thumbnail);
+
+
+    }
+
+
+    private void sharePhotoToFacebook(Bitmap imagePath) {
 
         SharePhoto photo = new SharePhoto.Builder()
-                .setBitmap(image)
+                .setBitmap(imagePath)
                 .setCaption("Nice Place.....")
                 .build();
 
@@ -148,9 +310,12 @@ public class Homepage extends AppCompatActivity implements PopupMenu.OnMenuItemC
                 .addPhoto(photo)
                 .build();
 
-        ShareApi.share(content, null);
+        shareDialog.show(content);
 
-        Toast.makeText(Homepage.this, "Facebook Photo Upload Complated", Toast.LENGTH_LONG).show();
+        ShareApi.share(content, null);
+        testing_if_sharing_successful();
+
+        Toast.makeText(Homepage.this, "Facebook Photo Upload Completed", Toast.LENGTH_LONG).show();
 
     }
 
@@ -232,7 +397,9 @@ public class Homepage extends AppCompatActivity implements PopupMenu.OnMenuItemC
 
             case R.id.Facebook:
                 result = true;
-                sharePhotoToFacebook();
+                selectImage();
+
+
                 break;
 
 
@@ -358,16 +525,22 @@ public class Homepage extends AppCompatActivity implements PopupMenu.OnMenuItemC
     }
 
 
+
+
+    //Unit testing out puts successful to logcat if sharing is successful.
+    public void testing_if_sharing_successful(){
+        Log.e("Sharing","Successful");
+
+
+    }
+
+
     //Unit tests to check if button is pushed or not during runtime
     //personal test method used while building the application.
     public boolean test_if_button_pushed() {
         boolean value = false;
 
-        if (!testing) {
-            value = false;
-        } else {
-            value = true;
-        }
+        value = testing;
 
         return value;
 
@@ -409,4 +582,5 @@ public class Homepage extends AppCompatActivity implements PopupMenu.OnMenuItemC
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
     }
+
 }
--- app/src/main/java/com/umenu/umenu/MainActivity.java
+++ app/src/main/java/com/umenu/umenu/MainActivity.java
@@ -1,17 +1,84 @@
 package com.umenu.umenu;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+public class MainActivity extends Activity{
+    CallbackManager callbackManager;
 
     @Override
    synchronized protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        facebookSDKInitialize();
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email,publish_actions");
+        getLoginDetails(loginButton);
+
+
     }
+
+
+
+
+    /*
+    Initialize the facebook sdk and then callback manager will handle the login responses.
+ */
+    protected void facebookSDKInitialize() {
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+
+        /*
+    Register a callback function with LoginButton to respond to the login result.
+    On successful login,login result has new access token and  recently granted permissions.
+    */
+
+    protected void getLoginDetails(LoginButton login_button){
+
+        // Callback registration
+        login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult login_result) {
+
+
+                getUserInfo(login_result);
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                // code for cancellation
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                //  code to handle error
+            }
+        });
+    }
+
+
 
     public void onButtonClick(View v)
     {
@@ -28,4 +95,79 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    protected void getUserInfo(LoginResult login_result){
+
+        GraphRequest data_request = GraphRequest.newMeRequest(
+                login_result.getAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject json_object,
+                            GraphResponse response) {
+                        Intent intent = new Intent(MainActivity.this,Homepage.class);
+                        intent.putExtra("jsondata",json_object.toString());
+                        startActivity(intent);
+                    }
+                });
+        Bundle permission_param = new Bundle();
+        permission_param.putString("fields", "id,name,email,picture.width(120).height(120)");
+        data_request.setParameters(permission_param);
+        data_request.executeAsync();
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+        @Override
+        protected void onResume(){
+            super.onResume();
+
+            // Logs 'install' and 'app activate' App Events.
+
+        }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+
+    }
+
+
+
+
+
+
+
+
+
 }
