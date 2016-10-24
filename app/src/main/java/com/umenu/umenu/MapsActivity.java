4e06249e67259dd37722ee3869ffaade40c60b45
jqj8595 <jqj8595@autuni.ac.nz>
2016-10-24 21:36:05 +1300

    Unit tests added for google map activity

--- app/src/main/java/com/umenu/umenu/MapsActivity.java
+++ app/src/main/java/com/umenu/umenu/MapsActivity.java
@@ -2,31 +2,23 @@ package com.umenu.umenu;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.nfc.Tag;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.location.places.PlaceLikelihood;
-import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
-import com.google.android.gms.location.places.Places;
-import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.plus.Plus;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    public boolean google_api =false; //Used for unit testing
+    private static final String TAG = "GoogleMapLocation";
 
 
     public String ACCESS_FINE_LOCATION = "android.permission.ACCESS_FINE_LOCATION";
@@ -70,8 +62,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         // LatLng sydney = new LatLng(-34, 151);
-        LatLng auckland = new LatLng(-36.870717, 174.746929);
-        mMap.addMarker(new MarkerOptions().position(auckland).title("Kingsland"));
+        LatLng auckland = new LatLng(-36.853232, 174.767318);
+        mMap.addMarker(new MarkerOptions().position(auckland).title("AUT UNIVERSITY"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(auckland));
 
         //mMap.setMyLocationEnabled(true);
@@ -91,10 +83,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         startActivity(mapIntent);
+        google_api = true;
+
+
 
 
 
     }
+
+
+
 
     @Override
     public void onStart() {
@@ -135,4 +133,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         AppIndex.AppIndexApi.end(client, viewAction);
         client.disconnect();
     }
+
+
+
+        /**
+         * Unit tests: checks to see if the google api is running successfully and if the intent
+         * has began.
+         * @return boolean
+         */
+        public boolean isGoogle_api_running(){
+            if(google_api){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+
+
+        /**
+         * Unit tests checks to see if desired location is correct and it is showing
+         * nearby locations of restaurants being searched.
+         * takes in the URI key word and prints to log cat for developer to see in terminal
+         * if restaurants can be found or not while app is running.
+         * @param uri
+         */
+
+        public void is_location_correct(Uri uri){
+            if(uri.equals("geo:0,0?q=restaurants")){
+                Log.e(TAG, "Restaurants can be found near by" );
+
+            }
+            else{
+                Log.e(TAG, "Restaurants can not be located nearby");
+            }
+        }
+
+
+
 }
